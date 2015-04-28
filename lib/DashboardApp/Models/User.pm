f1a5f0838ca3a23ecdc07f4c04d9b71f74ae8a47
Ivan Kapelyukhin <i.kapelyukhin@wsmintl.com>
2015-04-28 12:54:17 -0400

    Small fix

--- lib/DashboardApp/Models/User.pm
+++ lib/DashboardApp/Models/User.pm
@@ -10,7 +10,7 @@ my $users = LoadFile("users.yaml");
 sub check {
     my ( $login, $password ) = @_;
     
-    return $users->{role} if ( defined $users->{ $login } and $users->{ $login }->{password} eq $password );
+    return $users->{ $login }->{role} if ( defined $users->{ $login } and $users->{ $login }->{password} eq $password );
 }
 
 sub get_rt_creds {
