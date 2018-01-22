2785614a9694767c5dc009dada5aff9dd7862e98
Jesse Weaver <pianohacker@gmail.com>
2018-01-22 16:36:59 -0700

    Add loading indicator to dashboard

--- ui/view/my-tickets.js
+++ ui/view/my-tickets.js
@@ -14,6 +14,10 @@ import List, {
     ListItemText,
 } from 'material-ui/List';
 
+import {
+    LinearProgress
+} from 'material-ui/Progress';
+
 import React from 'react';
 import { connect } from 'react-redux';
 
@@ -32,27 +36,29 @@ class MyTickets extends React.Component {
     }
 
     render() {
-        const { classes, dispatch, employee: { columns = {} } } = this.props;
+        const { classes, dispatch, employee: { columns = {} }, loading } = this.props;
 
         let orderedColumns = Object.values( columns );
         orderedColumns.sort( ( a, b ) => a.column_order - b.column_order );
 
         return <div className={ classes.page }>
-            <Grid container spacing={24}>
-                { orderedColumns.map( column => <Grid item xs={12} sm={4} md={2} key={column.name}>
-                    <Card>
-                        <CardHeader title={column.name} />
-                        <CardContent>
-                            <Divider />
-                            <List>
-                                { column.tickets.map( ticketID => <TicketItem key={ticketID} ticketID={ticketID} /> ) }
-                            </List>
-                        </CardContent>
-                    </Card>
-                </Grid> ) }
-            </Grid>
+            { loading ? <LinearProgress /> :
+                <Grid container spacing={24}>
+                    { orderedColumns.map( column => <Grid item xs={12} sm={4} md={2} key={column.name}>
+                        <Card>
+                            <CardHeader title={column.name} />
+                            <CardContent>
+                                <Divider />
+                                <List>
+                                    { column.tickets.map( ticketID => <TicketItem key={ticketID} ticketID={ticketID} /> ) }
+                                </List>
+                            </CardContent>
+                        </Card>
+                    </Grid> ) }
+                </Grid>
+            }
         </div>;
     }
 }
 
-export default connectWithStyles( ( { employee } ) => ( { employee } ) )( MyTickets );
+export default connectWithStyles( ( { inProgress, employee } ) => ( { employee, loading: !!inProgress.GET_DASHBOARD } ) )( MyTickets );
