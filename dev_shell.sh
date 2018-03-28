21d2c288f3df2f51e2ef48ecd996e3295cbcc585
Jesse Weaver <pianohacker@gmail.com>
2018-03-27 19:04:37 -0600

    Make dev_shell dependent on Perl, not non-core Python module

--- dev_shell.sh
+++ dev_shell.sh
@@ -2,14 +2,13 @@
 
 function get_config {
     local path=$1
-
-    python -c 'import yaml; conf=yaml.load(open("config/config.yaml")); print conf'$path
+    perl -MYAML -e 'print YAML::LoadFile("config/config.yaml")->'$path
 }
 
 export DBIC_MIGRATION_SCHEMA_CLASS=DashboardApp::Schema
-export DBIC_MIGRATION_DSN=$(get_config '["db"]["dsn"]')
-export DBIC_MIGRATION_USERNAME=$(get_config '["db"]["username"]')
-export DBIC_MIGRATION_PASSWORD=$(get_config '["db"]["password"]')
+export DBIC_MIGRATION_DSN=$(get_config '{db}->{dsn}')
+export DBIC_MIGRATION_USERNAME=$(get_config '{db}->{username}')
+export DBIC_MIGRATION_PASSWORD=$(get_config '{db}->{password}')
 export PERL5LIB=./local/lib/perl5/:.
 export PATH="$PATH:./local/bin"
 exec $SHELL
